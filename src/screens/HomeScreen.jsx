8367dd5060e6afffbf5708a4a8e14e80abd75f4e
fernandovidalpiazza <fervidalpiazza@gmail.com>
2023-10-30 18:28:46 -0300

    primeros archivos

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native"
+
+
+const HomeScreen = () => {
+  return (
+   < View>
+   <Text>
+    estoy en home de pokedex
+    </Text>
+    </View>
+  )
+}
+
+export default HomeScreen
